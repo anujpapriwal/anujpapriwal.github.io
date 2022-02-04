6a03c3816073f1d49671d4d01ba6e5dfe40ee397
Anuj Papriwal <anuj.papriwal@gmail.com>
2022-02-05 01:28:40 +0530

    color changes

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Please check again after some time hopefully there will be 
+          something here, if I feel motivated enough.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
